2f689cba5f2223e8e786c92dbfac3016fb212340
Thomas Roch <thomas.c.roch@gmail.com>
2018-05-09 21:04:00 +0100

    Add refract-react-callbag package

---
+++ packages/refract-react-callbag/src/__tests__/index.ts
@@ -0,0 +1,42 @@
+import {
+    withEffects,
+    EffectHandler,
+    EffectFactory,
+    ObservableComponent
+} from '../index'
+import { map } from 'rxjs/operators'
+
+describe('refract-react-rxjs', () => {
+    interface Effect {
+        type: string
+        value: number
+    }
+    interface Props {
+        value: number
+    }
+
+    const noop = (...args) => void 0
+
+    const effectHandler: EffectHandler<Props, Effect> = props => (
+        value: Effect
+    ) => {
+        noop(value)
+    }
+
+    const effectFactory: EffectFactory<Props, Effect> = props => component => {
+        const value$ = component.observe<number>('value')
+
+        return value$.pipe(
+            map(value => ({
+                type: 'MyEffect',
+                value
+            }))
+        )
+    }
+
+    const withEffectsHOC = withEffects<Props, Effect>(effectHandler)
+
+    it('should work', () => {
+        const WithEffects = withEffectsHOC(effectFactory)
+    })
+})
---
+++ packages/refract-react-callbag/src/observable.ts
@@ -0,0 +1,43 @@
+import * as fromObs from 'callbag-from-obs'
+import * as toObs from 'callbag-to-obs'
+import { Callbag } from 'callbag'
+
+export interface Listener<T> {
+    next: (val: T) => void
+    error: (err: any) => void
+    complete: (val?: T) => void
+}
+
+export interface Subscription {
+    unsubscribe(): void
+}
+
+export interface ObservableComponent {
+    observe: <T>(propName: string) => Callbag
+    mount: Callbag
+    unmount: Callbag
+}
+
+export type EffectFactory<P, E> = (
+    props: P
+) => (component: ObservableComponent) => Callbag
+
+export const subscribeToSink = <T>(
+    sink: Callbag,
+    next: (val: T) => void
+): Subscription =>
+    toObs(sink).subscribe({
+        next
+    })
+
+export const createObservable = <T>(subscribe): Callbag => {
+    const observable = {
+        subscribe(listener: Listener<T>) {
+            const unsubscribe = subscribe(listener)
+
+            return { unsubscribe }
+        }
+    }
+
+    return fromObs(observable)
+}
