1edd70c65df22a0fb637b9673082e3c5d885a7de
Thomas Roch <thomas.c.roch@gmail.com>
2018-05-09 11:02:47 +0100

    Configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import typescript from 'rollup-plugin-typescript2'
+
+const packages = ['refract-react-rxjs']
+const formats = {
+    es: 'index.es.js',
+    cjs: 'index.js'
+}
+
+export default packages
+    .map(packageName => {
+        const packageDir = `packages/${packageName}`
+        const dependencies = Object.keys(
+            require(`./${packageDir}/package.json`).dependencies || {}
+        )
+        const peerDependencies = Object.keys(
+            require(`./${packageDir}/package.json`).peerDependencies || {}
+        )
+        const externalDependencies = dependencies.concat(peerDependencies)
+
+        return Object.keys(formats).map(format => ({
+            input: `${packageDir}/src/index.ts`,
+            plugins: [
+                typescript({
+                    tsconfig: `./${packageDir}/tsconfig.json`,
+                    useTsconfigDeclarationDir: true,
+                    clean: true
+                })
+            ],
+            external: externalDependencies,
+            output: {
+                name: 'refract',
+                format,
+                file: `${packageDir}/${formats[format]}`
+            }
+        }))
+    })
+    .reduce((rollupConfig, configs) => rollupConfig.concat(configs, []))
