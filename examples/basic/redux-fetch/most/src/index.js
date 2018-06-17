6a0c045b0a3b28d17aaba7a509198edee5358d99
Joe McGrath <joe.mcgrath@fanduel.com>
2018-06-17 19:35:43 +0100

    chore: fix broken example
    need to read more carefully - most combine requires a function as its first argument!

--- examples/basic/redux-fetch/most/src/index.js
+++ examples/basic/redux-fetch/most/src/index.js
@@ -9,50 +9,48 @@ import { actionCreators, actionTypes, selectors } from './store'
 import store from './setupStore'
 
 const effectHandler = ({ store }) => effect => {
-    console.log(effect)
-    // if (effect.type === actionTypes.ERROR_RECEIVE) {
-    //     console.log(effect)
-    // }
+    if (effect.type === actionTypes.ERROR_RECEIVE) {
+        console.log(effect)
+    }
 
-    // if (effect.type === actionTypes.USER_RECEIVE) {
-    //     store.dispatch(effect)
-    // }
+    if (effect.type === actionTypes.USER_RECEIVE) {
+        store.dispatch(effect)
+    }
 
-    // if (effect.type === actionTypes.USER_SELECT) {
-    //     store.dispatch(effect)
-    // }
+    if (effect.type === actionTypes.USER_SELECT) {
+        store.dispatch(effect)
+    }
 }
 
-const effectFactory = ({ store }) => () => {
+const effectFactory = ({ store }) => component => {
     const combined$ = combine(
+        (x, y) => [x, y],
         store.observe(actionTypes.USER_REQUEST),
         store.observe(selectors.getUsers)
     )
 
-    return combined$
+    const requestUser$ = combined$
+        .filter(([request, users]) => !Boolean(users[request.payload]))
+        .chain(([{ payload: username }]) =>
+            fromPromise(
+                fetch(`https://api.github.com/users/${username}`).then(res =>
+                    res.json()
+                )
+            )
+        )
+        .map(
+            ({ message, ...response }) =>
+                Boolean(message)
+                    ? actionCreators.receiveError(message)
+                    : actionCreators.receiveUser(response)
+        )
 
-    // const requestUser$ = combined$
-    //     .filter(([request, users]) => !Boolean(users[request.payload]))
-    //     .map(([{ payload: username }]) =>
-    //         fromPromise(
-    //             fetch(`https://api.github.com/users/${username}`).then(res =>
-    //                 res.json()
-    //             )
-    //         )
-    //     )
-    //     .flatten.map(
-    //         ({ message, ...response }) =>
-    //             Boolean(message)
-    //                 ? actionCreators.receiveError(message)
-    //                 : actionCreators.receiveUser(response)
-    //     )
+    const selectUser$ = combined$
+        .filter(([request, users]) => Boolean(users[request.payload]))
+        .map(([{ payload }]) => payload)
+        .map(actionCreators.selectUser)
 
-    // const selectUser$ = combined$
-    //     .filter(([request, users]) => Boolean(users[request.payload]))
-    //     .map(([{ payload }]) => payload)
-    //     .map(actionCreators.selectUser)
-
-    // return merge(requestUser$, selectUser$)
+    return merge(requestUser$, selectUser$)
 }
 
 const App = withEffects(effectHandler)(effectFactory)(Layout)
