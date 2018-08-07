a78058ded8b4daa60495dd277ee44b15605ddd38
Thomas Roch <thomas.c.roch@gmail.com>
2018-08-07 21:22:38 +0200

    Add signal capabilities

--- base/react/observable_most.ts
+++ base/react/observable_most.ts
@@ -5,6 +5,7 @@ export { Listener }
 
 export interface ObservableComponent {
     observe: <T>(propName: string) => Stream<T>
+    signal: <T>(signalName: string) => Stream<T>
     mount: Stream<any>
     unmount: Stream<any>
 }
--- base/react/observable_xstream.ts
+++ base/react/observable_xstream.ts
@@ -4,6 +4,7 @@ export { Listener, Subscription }
 
 export interface ObservableComponent {
     observe: <T>(propName: string) => Stream<T>
+    signal: <T>(signalName: string) => Stream<T>
     mount: Stream<any>
     unmount: Stream<any>
 }
