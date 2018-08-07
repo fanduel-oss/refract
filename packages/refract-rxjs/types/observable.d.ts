772ec4f40225440b26407a3325883da12e47e1b1
Thomas Roch <thomas.c.roch@gmail.com>
2018-08-07 16:27:33 +0200

    Update type definition

--- packages/refract-rxjs/types/observable.d.ts
+++ packages/refract-rxjs/types/observable.d.ts
@@ -1,12 +1,12 @@
 import { Observable, PartialObserver as Listener, Subscription } from 'rxjs'
 export { Listener, Subscription }
 export interface ObservableComponent {
-    observe: <T>(propName: string) => Observable<T>
+    observe: <T>(propName?: string) => Observable<T>
     mount: Observable<any>
     unmount: Observable<any>
 }
 export declare type Aperture<P, E> = (
-    props: P
+    initialProps: P
 ) => (component: ObservableComponent) => Observable<E>
 export declare const subscribeToSink: <T>(
     sink: Observable<T>,
