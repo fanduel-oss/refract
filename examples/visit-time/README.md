07b050871a814e7033a9712ea728f2f4ec44ff3b
Thomas Roch <thomas.c.roch@gmail.com>
2018-08-29 20:42:06 +0100

    Update repo org and package fields

--- examples/visit-time/README.md
+++ examples/visit-time/README.md
@@ -5,7 +5,7 @@
 <!-- prettier-ignore-start -->
 | callbag | most | RxJS | xstream |
 | --- | --- | --- | --- |
-| [`code`](./callbag) [`live`](https://codesandbox.io/s/github/troch/refract/tree/master/examples/visit-time/callbag) | [`code`](./most) [`live`](https://codesandbox.io/s/github/troch/refract/tree/master/examples/visit-time/most)  | [`code`](./rxjs) [`live`](https://codesandbox.io/s/github/troch/refract/tree/master/examples/visit-time/rxjs)  | [`code`](./xstream) [`live`](https://codesandbox.io/s/github/troch/refract/tree/master/examples/visit-time/xstream)  |
+| [`code`](./callbag) [`live`](https://codesandbox.io/s/github/fanduel-oss/refract/tree/master/examples/visit-time/callbag) | [`code`](./most) [`live`](https://codesandbox.io/s/github/fanduel-oss/refract/tree/master/examples/visit-time/most)  | [`code`](./rxjs) [`live`](https://codesandbox.io/s/github/fanduel-oss/refract/tree/master/examples/visit-time/rxjs)  | [`code`](./xstream) [`live`](https://codesandbox.io/s/github/fanduel-oss/refract/tree/master/examples/visit-time/xstream)  |
 <!-- prettier-ignore-end -->
 
 This examples renders a stopwatch counting the time a user spends on a page while being online.
@@ -14,8 +14,8 @@ This examples renders a stopwatch counting the time a user spends on a page whil
 
 Two sources observed:
 
--   The visibility status of the page (See [page visibility API](https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API)
--   The online status of the navigator (See [online and offline events](https://developer.mozilla.org/en-US/docs/Web/API/NavigatorOnLine/Online_and_offline_events)
+*   The visibility status of the page (See [page visibility API](https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API)
+*   The online status of the navigator (See [online and offline events](https://developer.mozilla.org/en-US/docs/Web/API/NavigatorOnLine/Online_and_offline_events)
 
 Every time the page becomes visible and online, the stopwatch resumes. If the page becomes either offline or hidden, the stopwatch pauses.
 
@@ -27,5 +27,5 @@ The Refract `handler` handles `pause`, `resume` and `tick` effects to track the 
 
 The end result is a stopwatch counting the number of milliseconds spent on the page when visible and online. To play with it you can:
 
--   switch between tabs, and you'll notice the counter hasn't moved while the example tab was hidden
--   toggle your navigator online status (can be done via devtools)
+*   switch between tabs, and you'll notice the counter hasn't moved while the example tab was hidden
+*   toggle your navigator online status (can be done via devtools)
--- scripts/generatePkg.js
+++ scripts/generatePkg.js
@@ -6,38 +6,68 @@ const writeFile = util.promisify(fs.writeFile)
 const mkdir = util.promisify(fs.mkdir)
 
 const getPackages = require('../packages')
+const libs = {
+    react: 'React',
+    inferno: 'Inferno',
+    preact: 'Preact',
+    redux: 'Redux',
+    xstream: 'xstream',
+    rxjs: 'RxJS',
+    most: 'Most',
+    callbag: 'Callbag'
+}
 
 generatePackages()
 
 async function generatePackages() {
     try {
         const packageBase = require('../base/all/package.base.json')
-        getPackages().map(async ({ name, dependencies, peerDependencies }) => {
-            let existingPackage
-
-            await mkdir(path.join(__dirname, '..', 'packages', name)).catch(
-                () => {}
-            )
-
-            try {
-                existingPackage = require(`../packages/${name}/package.json`)
-            } catch (e) {
-                existingPackage = { version: '0.0.0' }
-            }
-
-            const finalPackage = {
+        getPackages().map(
+            async ({
                 name,
-                ...existingPackage,
-                ...packageBase,
-                peerDependencies,
-                ...(Object.keys(dependencies).length ? { dependencies } : {})
-            }
+                obsLib,
+                mainLib,
+                dependencies,
+                peerDependencies
+            }) => {
+                let existingPackage
 
-            await writeFile(
-                path.join(__dirname, '..', 'packages', name, 'package.json'),
-                JSON.stringify(finalPackage, null, 4) + '\n'
-            )
-        })
+                await mkdir(path.join(__dirname, '..', 'packages', name)).catch(
+                    () => {}
+                )
+
+                try {
+                    existingPackage = require(`../packages/${name}/package.json`)
+                } catch (e) {
+                    existingPackage = { version: '0.0.0' }
+                }
+
+                const finalPackage = {
+                    name,
+                    ...existingPackage,
+                    ...packageBase,
+                    peerDependencies,
+                    ...(Object.keys(dependencies).length
+                        ? { dependencies }
+                        : {}),
+                    description: `Refract bindings for ${libs[mainLib]} with ${
+                        libs[obsLib]
+                    }: master your app effects reactively!`,
+                    keywords: packageBase.keywords.concat([mainLib, obsLib])
+                }
+
+                await writeFile(
+                    path.join(
+                        __dirname,
+                        '..',
+                        'packages',
+                        name,
+                        'package.json'
+                    ),
+                    JSON.stringify(finalPackage, null, 4) + '\n'
+                )
+            }
+        )
     } catch (e) {
         console.error(e.toString())
     }
