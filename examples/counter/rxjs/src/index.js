e845c5f075a3699baca88c7b284e8b2ed0a99793
Joe McGrath <joe.mcgrath@fanduel.com>
2018-09-01 21:44:10 +0100

    Update rxjs counter example

--- examples/counter/rxjs/src/index.js
+++ examples/counter/rxjs/src/index.js
@@ -1,39 +1,37 @@
 import React from 'react'
 import { render } from 'react-dom'
-import withState from 'react-state-hoc'
+
 import { withEffects } from 'refract-rxjs'
 import { interval } from 'rxjs/observable/interval'
 import { withLatestFrom, map } from 'rxjs/operators'
 
+import StateContainer from './StateContainer'
 import Layout from './Layout'
-
-const handler = props => effect => {
-    if (effect.type === 'INCREASE') {
-        props.setState(({ count }) => ({ count: count + 1 }))
-    }
-
-    if (effect.type === 'DECREASE') {
-        props.setState(({ count }) => ({ count: count - 1 }))
-    }
-}
 
 const aperture = props => component => {
     const direction$ = component.observe('direction')
-
     const tick$ = interval(1000)
 
-    return tick$.pipe(
-        withLatestFrom(direction$),
-        map(([, type]) => ({ type }))
-    )
+    return tick$.pipe(withLatestFrom(direction$), map(([, type]) => ({ type })))
 }
 
-const initialState = { count: 0, direction: 'NONE' }
+const handler = props => effect => {
+    switch (effect.type) {
+        case 'DECREASE':
+            return props.setState(({ count }) => ({ count: count - 1 }))
 
-const mapSetStateToProps = { setDirection: direction => ({ direction }) }
+        case 'INCREASE':
+            return props.setState(({ count }) => ({ count: count + 1 }))
 
-const App = withState(initialState, mapSetStateToProps)(
-    withEffects(handler)(aperture)(Layout)
+        default:
+            return
+    }
+}
+
+const LayoutWithEffects = withEffects(handler)(aperture)(Layout)
+
+const App = () => (
+    <StateContainer>{state => <LayoutWithEffects {...state} />}</StateContainer>
 )
 
 render(<App />, document.getElementById('root'))
