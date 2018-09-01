8871e02bdadd7176c766c76c322895a320b254b6
Joe McGrath <joe.mcgrath@fanduel.com>
2018-09-01 21:38:27 +0100

    Update most counter example

--- examples/counter/most/src/index.js
+++ examples/counter/most/src/index.js
@@ -1,35 +1,36 @@
 import React from 'react'
 import { render } from 'react-dom'
-import withState from 'react-state-hoc'
+
 import { withEffects } from 'refract-most'
 import { combine, periodic } from 'most'
 
+import StateContainer from './StateContainer'
 import Layout from './Layout'
-
-const handler = props => effect => {
-    if (effect.type === 'INCREASE') {
-        props.setState(({ count }) => ({ count: count + 1 }))
-    }
-
-    if (effect.type === 'DECREASE') {
-        props.setState(({ count }) => ({ count: count - 1 }))
-    }
-}
 
 const aperture = props => component => {
     const direction$ = component.observe('direction')
-
     const tick$ = periodic(1000)
 
     return combine(type => ({ type }), direction$, tick$)
 }
 
-const initialState = { count: 0, direction: 'NONE' }
+const handler = props => effect => {
+    switch (effect.type) {
+        case 'DECREASE':
+            return props.setState(({ count }) => ({ count: count - 1 }))
 
-const mapSetStateToProps = { setDirection: direction => ({ direction }) }
+        case 'INCREASE':
+            return props.setState(({ count }) => ({ count: count + 1 }))
 
-const App = withState(initialState, mapSetStateToProps)(
-    withEffects(handler)(aperture)(Layout)
+        default:
+            return
+    }
+}
+
+const LayoutWithEffects = withEffects(handler)(aperture)(Layout)
+
+const App = () => (
+    <StateContainer>{state => <LayoutWithEffects {...state} />}</StateContainer>
 )
 
 render(<App />, document.getElementById('root'))
