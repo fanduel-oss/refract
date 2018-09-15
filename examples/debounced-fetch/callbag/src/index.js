cec04eb1fce1d65c032039b13c182c2cab883398
Joe McGrath <joe.mcgrath@fanduel.com>
2018-09-15 18:46:06 +0100

    Refactor debounced-fetch example

--- examples/debounced-fetch/callbag/src/index.js
+++ examples/debounced-fetch/callbag/src/index.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
-import withState from 'react-state-hoc'
+
 import { withEffects } from 'refract-callbag'
 import fromPromise from 'callbag-from-promise'
 import { debounce } from 'callbag-debounce'
 import { filter, flatten, map, pipe } from 'callbag-basics'
 
+import StateContainer from './StateContainer'
 import Layout from './Layout'
 
-const handler = ({ setState }) => effect => {
-    if (effect.type === 'USER_DATA_RECEIVE') {
-        setState({ data: effect.payload })
-    }
-}
-
-const aperture = () => ({ observe }) =>
+const aperture = props => ({ observe }) =>
     pipe(
         observe('username'),
         filter(Boolean),
@@ -30,12 +25,20 @@ const aperture = () => ({ observe }) =>
         map(payload => ({ type: 'USER_DATA_RECEIVE', payload }))
     )
 
-const initialState = { data: null, username: '' }
+const handler = ({ setState }) => effect => {
+    switch (effect.type) {
+        case 'USER_DATA_RECEIVE':
+            return setState({ data: effect.payload })
 
-const mapSetStateToProps = { setUsername: username => ({ username }) }
+        default:
+            return
+    }
+}
 
-const App = withState(initialState, mapSetStateToProps)(
-    withEffects(handler)(aperture)(Layout)
+const LayoutWithEffects = withEffects(handler)(aperture)(Layout)
+
+const App = () => (
+    <StateContainer>{state => <LayoutWithEffects {...state} />}</StateContainer>
 )
 
 render(<App />, document.getElementById('root'))
