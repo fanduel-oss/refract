fanduel-oss/refract

commit d24f0f099b2d89b9549b1b6e6ee079e67f17873a

Distinguish between a seed value of undefined and no seed value

base/react/observable.ts
@@ -87,14 +87,15 @@ const getComponentBase = (
             })
         )
 
-    const useEvent = (eventName: string, seedValue?: any) => {
+    const useEvent = (...args) => {
+        const eventName: string = args[0]
+        const hasSeedValue = args.length > 1
+        const seedValue = args[2]
         const events$ = fromEvent(eventName)
         const pushEventValue = pushEvent(eventName)
 
         return [
-            seedValue === undefined
-                ? events$
-                : events$.pipe(startWith(seedValue)),
+            !hasSeedValue ? events$ : events$.pipe(startWith(seedValue)),
             pushEventValue
         ]
     }

base/react/observable_callbag.ts
@@ -90,14 +90,15 @@ const getComponentBase = (
             })
         )
 
-    const useEvent = (eventName: string, seedValue?: any) => {
+    const useEvent = (...args) => {
+        const eventName: string = args[0]
+        const hasSeedValue = args.length > 1
+        const seedValue = args[2]
         const events$ = fromEvent(eventName)
         const pushEventValue = pushEvent(eventName)
 
         return [
-            seedValue === undefined
-                ? events$
-                : pipe(events$, startWith(seedValue)),
+            !hasSeedValue ? events$ : pipe(events$, startWith(seedValue)),
             pushEventValue
         ]
     }

base/react/observable_most.ts
@@ -74,12 +74,15 @@ const getComponentBase = (
             return valueTransformer ? valueTransformer(value) : value
         })
 
-    const useEvent = (eventName: string, seedValue?: any) => {
+    const useEvent = (...args) => {
+        const eventName: string = args[0]
+        const hasSeedValue = args.length > 1
+        const seedValue = args[2]
         const events$ = fromEvent(eventName)
         const pushEventValue = pushEvent(eventName)
 
         return [
-            seedValue === undefined ? events$ : events$.startWith(seedValue),
+            !hasSeedValue ? events$ : events$.startWith(seedValue),
             pushEventValue
         ]
     }

base/react/observable_xstream.ts
@@ -71,12 +71,15 @@ const getComponentBase = (
             return valueTransformer ? valueTransformer(value) : value
         })
 
-    const useEvent = (eventName: string, seedValue?: any) => {
+    const useEvent = (...args) => {
+        const eventName: string = args[0]
+        const hasSeedValue = args.length > 1
+        const seedValue = args[2]
         const events$ = fromEvent(eventName)
         const pushEventValue = pushEvent(eventName)
 
         return [
-            seedValue === undefined ? events$ : events$.startWith(seedValue),
+            hasSeedValue ? events$ : events$.startWith(seedValue),
             pushEventValue
         ]
     }

commit 709258bfa455b35d2f311f65abd77d872ab0e7c9

Fix ObservableComponent::useEvent ignoring seed
ObservableComponent::useEvent checked whether
more than one argument was supplied (correct), but
used the 3rd argument as a seed value instead.
That argument is always undefined when using
Refract according to the docs.

This bug was introduced with commit d24f0f099b2.

This commit changes the function signature back
to the original one of using 2 args, one being
optional. The check for the actual argument count
is then done using the arguments object.

base/react/observable.ts
@@ -87,10 +87,8 @@ const getComponentBase = (
             })
         )
 
-    const useEvent = (...args) => {
-        const eventName: string = args[0]
-        const hasSeedValue = args.length > 1
-        const seedValue = args[2]
+    const useEvent = (eventName: string, seedValue?: any) => {
+        const hasSeedValue = arguments.length > 1
         const events$ = fromEvent(eventName)
         const pushEventValue = pushEvent(eventName)
 

base/react/observable_callbag.ts
@@ -90,10 +90,8 @@ const getComponentBase = (
             })
         )
 
-    const useEvent = (...args) => {
-        const eventName: string = args[0]
-        const hasSeedValue = args.length > 1
-        const seedValue = args[2]
+    const useEvent = (eventName: string, seedValue?: any) => {
+        const hasSeedValue = arguments.length > 1
         const events$ = fromEvent(eventName)
         const pushEventValue = pushEvent(eventName)
 

base/react/observable_most.ts
@@ -75,10 +75,8 @@ const getComponentBase = (
             return valueTransformer ? valueTransformer(value) : value
         })
 
-    const useEvent = (...args) => {
-        const eventName: string = args[0]
-        const hasSeedValue = args.length > 1
-        const seedValue = args[2]
+    const useEvent = (eventName: string, seedValue?: any) => {
+        const hasSeedValue = arguments.length > 1
         const events$ = fromEvent(eventName)
         const pushEventValue = pushEvent(eventName)
 

base/react/observable_xstream.ts
@@ -73,10 +73,8 @@ const getComponentBase = (
             return valueTransformer ? valueTransformer(value) : value
         })
 
-    const useEvent = (...args) => {
-        const eventName: string = args[0]
-        const hasSeedValue = args.length > 1
-        const seedValue = args[2]
+    const useEvent = (eventName: string, seedValue?: any) => {
+        const hasSeedValue = arguments.length > 1
         const events$ = fromEvent(eventName)
         const pushEventValue = pushEvent(eventName)
 